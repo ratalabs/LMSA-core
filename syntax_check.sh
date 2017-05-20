ca0a14780ee69174ead3d683990f1109488a2bc7
smccaffrey <smccaffrey70@gmail.com>
2017-05-20 15:11:21 -0700

    security fix

--- syntax_check.sh
+++ syntax_check.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if python -m py_compile /Users/smccaffrey/Desktop/git/PIRT_ASU/blackboard_automation/tests.py; then
+if python -m py_compile ~/Desktop/git/PIRT_ASU/blackboard_automation/tests.py; then
   echo "Exit code of 0: tests.py  --- SUCCESS"
 else
   echo "Exit code of $?, tests.py --- FAILURE"
 fi
 
-if python -m py_compile /Users/smccaffrey/Desktop/git/PIRT_ASU/blackboard_automation/assignments.py; then
+if python -m py_compile ~/Desktop/git/PIRT_ASU/blackboard_automation/assignments.py; then
   echo "Exit code of 0: assignments.py  --- SUCCESS"
 else
   echo "Exit code of $?, assignments.py --- FAILURE"
