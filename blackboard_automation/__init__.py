1ba3a27bdc7609071fdbe57d507e484411ffb7e7
smccaffrey <smccaffrey70@gmail.com>
2017-06-28 14:33:26 -0700

    update info

--- blackboard_automation/__init__.py
+++ blackboard_automation/__init__.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Sam McCaffrey"
+__copyright__ = "Copyright 2017, Sam McCaffrey"
+__license__ = "Apache-2.0"
+__version__ = "1.0.1"
+__maintainer__ = "Sam McCaffrey"
+__email__ = "samccaff@asu.edu"
+__status__ = "Production"
+
 from . import *
