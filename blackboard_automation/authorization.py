f44b25dc8e5a0e39329ebaa34aef317825f82433
smccaffrey <smccaffrey70@gmail.com>
2017-07-10 16:22:25 -0700

    added file

---
+++ blackboard_automation/authorization.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import getpass
+import time
+
+def __init__(self):
+    self.driver = driver
+    self.url = url
+
+def login(self, driver = driver, url = url):
+    try:
+        driver.find_element_by_xpath('//*[contains(@id, 'username')]')
+    except Exception as e:
+        print # coding=utf-8
+        raise
