55054b1b3765bc070d29bd75b835ef86cab99d5a
smccaffrey <smccaffrey70@gmail.com>
2017-07-14 01:32:16 -0700

    initialized logic directory

---
+++ blackboard_automation/logic/is_checked.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import selenium
+import time
+from selenium import webdriver as wbd
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+"""
+    Create an object to check the state of any boolean element
+"""
+class IsChecked(self, element):
+
+    def __init__(self):
+        self.element = element
+        
+    def is_checked(self, driver, item, **kwargs):
+        checked = driver.execute_script(("return document.getElementById('%s').checked") % item)
+        return checked
