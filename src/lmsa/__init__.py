3c722d9fc2401f59544106767f5ace144e335024
smccaffrey <smccaffrey70@gmail.com>
2017-07-21 21:17:00 -0700

    moved out of automation

--- src/lmsa/__init__.py
+++ src/lmsa/__init__.py
@@ -1,3 +1,18 @@
-"""Initialize core module. Contains all non-application functionality"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-#from .automation import *
+__author__ = "Sam McCaffrey"
+__copyright__ = "Copyright 2017, Sam McCaffrey"
+__license__ = "Apache-2.0"
+__maintainer__ = "Sam McCaffrey"
+__email__ = "samccaff@asu.edu"
+__status__ = "Production"
+
+
+from . import authorization
+from . import version
+from .editor import Editor
+from .forms import assignment_options
+from .forms.test_options import EditTests
+from .navigation.course.sidebar import SideBar
+from .navigation.home.section_selector import SectionSelector
