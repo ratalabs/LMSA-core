87e5f72fb3635d9b7f469bca1e0bd888b33e9f40
Ian Kenney <ian.kenney@asu.edu>
2017-07-29 22:45:12 -0700

    removed old files

--- src/lmsa/config/config.py
+++
@@ -1,8 +0,0 @@
-import ConfigParser
-import os
-
-def load_config(path=os.path.expanduser('~/.lmsa.cfg')):
-    config = ConfigParser.ConfigParser()
-    config.readfp('defaults.cfg')
-    config.read(path)
-    return config
