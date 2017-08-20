29e5c92f823528944741e3635cf23b592e8bdd80
smccaffrey <smccaffrey70@gmail.com>
2017-08-19 20:01:49 -0700

    rearranging *options.py

--- src/lmsa/lms/blackboard/options/assignment_options.py
+++ src/lmsa/lms/blackboard/options/assignment_options.py
@@ -6,16 +6,16 @@ Stores all full XPATHS relating to edit assignment options.
 #Need to map 'Grading Options' section
 #Need to map 'Display of Grades' section
 class assignment_options:
-    
-    DUE_DATE_CHECK = '//*[@id="due_date_in_use"]',
-    DUE_DATE_VALUE = '//*[@id="dp_dueDate_date"]',
-    DUE_DATE_TIME = '//*[@id="tp_dueDate_time"]',
-    POINTS_POSSIBLE = '//*[@id="possible"]',
-    MAKE_ASSIGNMENT_AVAILABLE = '//*[@id="isAvailable"]',
-    START_LIMIT_AVAILABILITY = '//*[@id="start_limitAvailability"]',
-    START_LIMIT_AVAILABILITY_DATE = '//*[@id="dp_limitAvailability_start_date"]',
-    START_LIMIT_AVAILABILITY_TIME = '//*[@id="tp_limitAvailability_start_time"]',
-    END_LIMIT_AVAILABILITY = '//*[@id="end_limitAvailability"]',
-    END_LIMIT_AVAILABILITY_DATE = '//*[@id="dp_limitAvailability_end_date"]',
-    END_LIMIT_AVAILABILITY_TIME = '//*[@id="tp_limitAvailability_end_time"]',
+
+    DUE_DATE_CHECK = '//*[@id="due_date_in_use"]'
+    DUE_DATE_VALUE = '//*[@id="dp_dueDate_date"]'
+    DUE_DATE_TIME = '//*[@id="tp_dueDate_time"]'
+    POINTS_POSSIBLE = '//*[@id="possible"]'
+    MAKE_ASSIGNMENT_AVAILABLE = '//*[@id="isAvailable"]'
+    START_LIMIT_AVAILABILITY = '//*[@id="start_limitAvailability"]'
+    START_LIMIT_AVAILABILITY_DATE = '//*[@id="dp_limitAvailability_start_date"]'
+    START_LIMIT_AVAILABILITY_TIME = '//*[@id="tp_limitAvailability_start_time"]'
+    END_LIMIT_AVAILABILITY = '//*[@id="end_limitAvailability"]'
+    END_LIMIT_AVAILABILITY_DATE = '//*[@id="dp_limitAvailability_end_date"]'
+    END_LIMIT_AVAILABILITY_TIME = '//*[@id="tp_limitAvailability_end_time"]'
     TRACK_NUMBER_OF_VIEWS_CHECK = '//*[@id="isTracked"]'
--- src/lmsa/lms/blackboard/options/test_options.py
+++ src/lmsa/lms/blackboard/options/test_options.py
@@ -4,24 +4,24 @@ Stores all full XPATHS relating to edit test options.
 
 class test_options:
 
-    OPEN_TEST_IN_NEW_WINDOW_YES = '//*[@id="yesRadio"]',
-    OPEN_TEST_IN_NEW_WINDOW_NO = '//*[@id="noRadio"]',
-    MAKE_LINK_AVAILABLE_YES = '//*[@id="fIsLinkVisible1"]',
-    MAKE_LINK_AVAILABLE_NO = '//*[@id="fIsLinkVisible2"]',
-    CREATE_ANNOUNCEMENT_YES = '//*[@id="fCreateAnnouncement1"]',
-    CREATE_ANNOUNCEMENT_NO = '//*[@id="fCreateAnnouncement2"]',
-    MULTIPLE_ATTEMPTS_CHECK = '//*[@id="fIsMultipleAttempts"]',
-    ALLOW_UNLIMITED_ATTEMPTS = '//*[@id="fIsUnlimitedAttempts"]',
-    NUMBER_OF_ATTEMPTS = '//*[@id="fNumMultipleAttempts"]',
-    NUMBER_OF_ATTEMPTS_VALUE = '//*[@id="attemptCount"]',
-    FORCE_COMPLETION_CHECK = '//*[@id="fIsForceComplete"]',
-    START_RESTRICT_CHECK = '//*[@id="start_restrict"]',
-    START_RESTRICT_DATE = '//*[@id="dp_restrict_start_date"]',
-    START_RESTRICT_TIME = '//*[@id="tp_restrict_start_time"]',
-    END_RESTRICT_CHECK = '//*[@id="end_restrict"]',
-    END_RESTRICT_DATE = '//*[@id="dp_restrict_end_date"]',
-    END_RESTRICT_TIME = '//*[@id="tp_restrict_end_time"]',
-    DUE_DATE_CHECK = '//*[@id="_dueDate"]',
-    DUE_DATE_DATE = '//*[@id="dp_dueDate_date"]',
-    DUE_DATE_TIME = '//*[@id="tp_dueDate_time"]',
+    OPEN_TEST_IN_NEW_WINDOW_YES = '//*[@id="yesRadio"]'
+    OPEN_TEST_IN_NEW_WINDOW_NO = '//*[@id="noRadio"]'
+    MAKE_LINK_AVAILABLE_YES = '//*[@id="fIsLinkVisible1"]'
+    MAKE_LINK_AVAILABLE_NO = '//*[@id="fIsLinkVisible2"]'
+    CREATE_ANNOUNCEMENT_YES = '//*[@id="fCreateAnnouncement1"]'
+    CREATE_ANNOUNCEMENT_NO = '//*[@id="fCreateAnnouncement2"]'
+    MULTIPLE_ATTEMPTS_CHECK = '//*[@id="fIsMultipleAttempts"]'
+    ALLOW_UNLIMITED_ATTEMPTS = '//*[@id="fIsUnlimitedAttempts"]'
+    NUMBER_OF_ATTEMPTS = '//*[@id="fNumMultipleAttempts"]'
+    NUMBER_OF_ATTEMPTS_VALUE = '//*[@id="attemptCount"]'
+    FORCE_COMPLETION_CHECK = '//*[@id="fIsForceComplete"]'
+    START_RESTRICT_CHECK = '//*[@id="start_restrict"]'
+    START_RESTRICT_DATE = '//*[@id="dp_restrict_start_date"]'
+    START_RESTRICT_TIME = '//*[@id="tp_restrict_start_time"]'
+    END_RESTRICT_CHECK = '//*[@id="end_restrict"]'
+    END_RESTRICT_DATE = '//*[@id="dp_restrict_end_date"]'
+    END_RESTRICT_TIME = '//*[@id="tp_restrict_end_time"]'
+    DUE_DATE_CHECK = '//*[@id="_dueDate"]'
+    DUE_DATE_DATE = '//*[@id="dp_dueDate_date"]'
+    DUE_DATE_TIME = '//*[@id="tp_dueDate_time"]'
     LATE_SUBMISSION_CHECK = '//*[@id="doNotAllowLateSubmission"]'
