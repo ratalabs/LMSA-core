bc79e0d064795a250863d77406d6ccc2cf202d46
smccaffrey <smccaffrey70@gmail.com>
2017-08-22 20:00:43 -0700

    testing due_dates_example.py

--- scripts/due_dates_example(PHY122).py
+++ scripts/due_dates_example(PHY122).py
@@ -6,7 +6,7 @@ from lmsa.manipulation.ASU.ASU_manipulator import ASU_manipulator
 from lmsa.lms.blackboard.BB_Editor import BB_Editor
 from selenium import webdriver
 
-filename = '/Users/smccaffrey/Downloads/LMSA-core-ce8d77a2dcc81854e0502902fdde6a175acfe800/PHY122_Fall2017/duedates_Fall2017.csv'
+filename = '/Users/smccaffrey/Desktop/PHY122_Fall2017_due_dates.csv'
 p = 'PHY 122: University Physics Lab I (2017 Fall)-'
 q = '2017Fall-T-PHY122-71848: PHY 122: University Physics Lab I (2017 Fall)'
 df1 = pd.read_csv(filename, dtype=str, delimiter=',', header=None)
@@ -29,6 +29,7 @@ time.sleep(4)
 i = 1
 for i in range(1, len(df1[0])):
     driver.find_element_by_link_text('2017Fall-T-PHY122-' + str(df1[0][i]) + ': PHY 122: University Physics Lab I (2017 Fall)').click()
+    print('Starting Section: ' + str(df1[0][i]))
 
     """Prelabs"""
     driver.find_element_by_link_text('Submit PreLab').click()
@@ -47,7 +48,7 @@ for i in range(1, len(df1[0])):
     for k in range(1, 11):
         form.select_form(df1[k+14][0], wait=1)
         form.edit(wait=5)
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") #quick fix for elementNotFound
         time.sleep(2)
         form.end_restrict(state=True, date=df1[k+14][i], time=df1[4][i])
         if not DRYRUN:
