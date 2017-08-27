6a6031da96d9ac0aee37760b38637c25f968759c
smccaffrey <smccaffrey70@gmail.com>
2017-08-26 23:06:58 -0700

    created imports_example.py

---
+++ scripts/imports_example.py
@@ -0,0 +1,19 @@
+
+"""Import functionality for navigating and logging into ASU
+"""
+from lmsa.manipulation.ASU.ASU_manipulator import ASU_manipulator
+
+"""Import the main BlackBoard Editor
+"""
+from lmsa.lms.blackboard.BlackBoard import Editor
+
+"""Import all the content editing modules
+"""
+from lmsa.lms.blackboard.content.tests import Tests
+from lmsa.lms.blackboard.content.assignments import Assignments
+from lmsa.lms.blackboard.content.folders import Folders
+
+"""Import Logic operations, general window manipulations
+"""
+from lmsa.lms.blackboard.Library import Logic
+from lmsa.lms.blackboard.Library import Window
