3ea5bc045c1b461259ca46e94bd5f4aa9545103c
smccaffrey <smccaffrey70@gmail.com>
2017-09-26 08:52:53 -0700

    vetting discussion board harvesting capabilities

--- scripts/discussion_board_example.py
+++ scripts/discussion_board_example.py
@@ -2,28 +2,19 @@ import sys
 import time
 sys.path.append('/Users/smccaffrey/Desktop/LMSA-core/src/')
 import pandas as pd
-from lmsa.manipulation.ASU.ASU_manipulator import ASU_manipulator
 from selenium import webdriver
 
-"""Import functionality for navigating and logging into ASU
-"""
 from lmsa.manipulation.ASU.ASU_manipulator import ASU_manipulator
-
-"""Import the main BlackBoard Editor
-"""
+from lmsa.manipulation.ASU.ASU_manipulator import ASU_manipulator
 from lmsa.lms.blackboard.BlackBoard import Editor
-
-"""Import all the content editing modules
-"""
 from lmsa.lms.blackboard.content.tests import Tests
 from lmsa.lms.blackboard.content.assignments import Assignments
 from lmsa.lms.blackboard.content.folders import Folders
-
-"""Import Logic operations, general window manipulations
-"""
 from lmsa.lms.blackboard.Library import Logic
 from lmsa.lms.blackboard.Library import Window
 
+"""Change when ready to save changes"""
+DRYRUN = True
 
 """
 filename = '/Users/smccaffrey/Desktop/PHY122_Fall2017_due_dates.csv'
@@ -33,9 +24,6 @@ df1 = pd.read_csv(filename, dtype=str, delimiter=',', header=None)
 driver = webdriver.Chrome('/Users/smccaffrey/Projects/drivers/chromedriver')
 BB_HOME = 'https://myasucourses.asu.edu/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_1_1'
 """
-
-"""Change when ready to save changes"""
-DRYRUN = True
 
 """Declare objects"""
 institution = ASU_manipulator(driver)
